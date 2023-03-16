1058ae9b9ca3c29253da83144aedc4a64ff2793c
salted1532 <salted1532@naver.com>
2023-03-16 16:46:56 +0900

    Ai 버그들 보안

--- My project/Assets/scripts/Enemysystem/EnemyAi.cs
+++ My project/Assets/scripts/Enemysystem/EnemyAi.cs
@@ -18,6 +18,7 @@ public class EnemyAi : MonoBehaviour
     public float timeBetweenAttacks;
     bool alreadyAttacked;
     public GameObject projectile;
+    public float waitTimer = 1f;
 
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
@@ -31,12 +32,14 @@ public class EnemyAi : MonoBehaviour
     seedspawner seedspawn;
     EnemyHealth EH;
     Player Pl;
+    Rigidbody rb;
 
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
         Pl = GameObject.Find("Player").GetComponent<Player>();
         EH = GetComponent<EnemyHealth>();
+        rb = GetComponent<Rigidbody>();
         agent = GetComponent<NavMeshAgent>();
         seedspawn = GameObject.Find("SeedSpawner").GetComponent<seedspawner>();
         enemy1.Shield = 10;
@@ -46,6 +49,10 @@ public class EnemyAi : MonoBehaviour
 
     private void Update()
     {
+        if(rb.velocity.magnitude > 25f)
+        {
+            rb.velocity = Vector3.zero;
+        }
         //플레이어 관측하고 공격하기
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
@@ -82,15 +89,22 @@ public class EnemyAi : MonoBehaviour
     {
         if (agent.remainingDistance <= agent.stoppingDistance)//목적지 도착 확인
         {
-            SearchWalkPoint();
+            rb.velocity = Vector3.zero;
+            waitTimer -= Time.deltaTime;
+            if (waitTimer <= 0)
+            {
+                SearchWalkPoint();
+                waitTimer = 0f;
+            }
         }
     }
     bool RandomPoint(Vector3 center, float range, out Vector3 result) //랜덤 위치 생성
     {
 
-        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        Vector3 randomPosition = Random.insideUnitSphere * 10f;
+        randomPosition += transform.position;
         NavMeshHit hit;
-        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        if (NavMesh.SamplePosition(randomPosition, out hit, 10f, NavMesh.AllAreas))
         {
             result = hit.position;
             return true;
